d4254641a04c1b2c58b1135157bffbca88a47daf
Antonie Lin <iammrhelo@gmail.com>
2017-02-19 14:31:17 +0800

    form index download and extract 10k save to csv

--- crawl10k.py
+++ crawl10k.py
@@ -1,13 +1,13 @@
 import argparse
 import csv
 from collections import namedtuple
-from multiprocessing import Process, Queue
+from itertools import product
 import os
 import requests
 
 from tqdm import tqdm
 
-SEC_GOV_URL = 'http://www.sec.gov/Archives/'
+SEC_GOV_URL = 'http://www.sec.gov/Archives'
 FORM_INDEX_URL = os.path.join(SEC_GOV_URL,'edgar','full-index','{}','QTR{}','form.idx')
 
 IndexRecord = namedtuple("IndexRecord",["form_type","company_name","cik","date_filed","filename"])
@@ -15,34 +15,53 @@ IndexRecord = namedtuple("IndexRecord",["form_type","company_name","cik","date_f
 class FormIndex(object):
     def __init__(self):
         self.formrecords = []
+        # Initialize cache directory
+        if not os.path.exists('./cache'):
+            os.makedirs("./cache/")
 
-    def download(self, year):
+    def retrieve(self, year, qtr):
+
+        form_idx = "form_year{}_qtr{}.index".format(year,qtr)
+        form_idx_path = os.path.join('.','cache',form_idx)
+
+        self.download(form_idx_path, year, qtr)
+        self.extract(form_idx_path)
+
+    def download(self, form_idx_path, year, qtr):
+        if os.path.exists(form_idx_path):
+            print("Download skipped: year {}, qtr {}".format(year,qtr))
+            return
+
+        # Download and save to cached directory
+        print("Downloading year {}, qtr {}".format(year,qtr))
+
+        index_url = FORM_INDEX_URL.format(year,qtr)
+        resp = requests.get(index_url)
+
+        with open(form_idx_path,'wb') as fout:
+            fout.write(resp.content)
+
+    def extract(self, form_idx_path):
         # Parse row to record
         def parse_row_to_record(row, fields_begin):
-            if len(fields_begin) == 0:
-                raise ValueError
-
             record = []
 
-            for cbegin, cend in zip(fields_begin[:],fields_begin[1:] + [len(row)] ):
-                field = row[cbegin:cend].rstrip()
+            for begin, end in zip(fields_begin[:],fields_begin[1:] + [len(row)] ):
+                field = row[begin:end].rstrip()
+                field = field.strip('\"')
                 record.append(field)
 
             return record
 
-        for qtr in range(1,2):
+        # Read and parse
+        print("Extracting from {}".format(form_idx_path))
 
-            index_url = FORM_INDEX_URL.format(year,qtr)
-            print("Downloading from {}".format(index_url))
+        with open(form_idx_path,'rb') as fin:
+            # If arrived at 10-K section of forms
+            arrived = False
 
-            resp = requests.get(index_url)
-
-            lines = resp.text.split('\n')
-
-
-            fields_begin = []
-
-            for row in lines:
+            for row in fin.readlines():
+                row = row.decode('ascii')
                 if row.startswith("Form Type"):
                     fields_begin = [ row.find("Form Type"),
                                      row.find("Company Name"),
@@ -50,11 +69,13 @@ class FormIndex(object):
                                      row.find('Date Filed'),
                                      row.find("File Name") ]
 
-                elif row.startswith("10-K"):
+                elif row.startswith("10-K "):
+                    arrived = True
                     rec = parse_row_to_record(row,fields_begin)
                     self.formrecords.append(IndexRecord(*rec))
 
-        #import pdb; pdb.set_trace()
+                elif arrived == True:
+                    break
 
     def save(self, path):
         print("Saving records to {}".format(path))
@@ -64,13 +85,54 @@ class FormIndex(object):
             for rec in self.formrecords:
                 writer.writerow( tuple(rec) )
 
+class Form(object):
+    def __init__(self):
+        pass
+
+    def download(self, form10k_savepath):
+
+        with open(form10k_savepath,'r') as fin:
+            reader = csv.reader(fin, delimiter=',')
+            for row in reader:
+                form_type, company_name, cik, date_filed, filename = row
+
+                url = os.path.join(SEC_GOV_URL,filename)
+
+                r = requests.get(url)
+
+
+
+
 def main():
     # Download form index
-    formindex = FormIndex()
-    path = 'form2006.index'
-    for year in range(2006,2007):
-        formindex.download(year)
-        formindex.save(path)
+    parser = argparse.ArgumentParser("Edgar 10k forms sentiment analysis")
+    parser.add_argument('--year_start',type=int,default=1993)
+    parser.add_argument('--year_end',type=int,default=2016)
+    args = parser.parse_args()
+    # Weird: 2011, QTR4
+    year_start = args.year_start
+    year_end = args.year_end
+
+    # Download and extract 10k forms
+    form10k_savepath = "year{}-{}.10k.csv".format(year_start,year_end)
+    if not os.path.exists(form10k_savepath):
+        formindex = FormIndex()
+        for year, qtr in product(range(args.year_start,args.year_end+1),range(1,5)):
+            formindex.retrieve(year, qtr)
+        formindex.save(form10k_savepath)
+    else:
+        print("{} already exists".format(form10k_savepath))
+
+
+    # Download 10k forms and extract MD&A
+    try:
+        os.makedirs('./mda')
+    except:
+        pass
+
+    form = Form()
+
+
 
 if __name__ == "__main__":
     main()
