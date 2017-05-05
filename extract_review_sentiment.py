966d2ac26c9000374639468aede2e32f86755958
Antonie Lin <iammrhelo@gmail.com>
2017-05-05 17:38:16 +0800

    statistical version

--- extract_review_sentiment.py
+++ extract_review_sentiment.py
@@ -1,68 +1,56 @@
 import argparse
 from glob import glob
 import os
-import re
 
+import numpy as np
 from tqdm import tqdm
 
 from encoder import Model
 
-def iter_file(mda_files, batch_size, paragraph=False):
-    if not paragraph:
-        for idx in range(0, len(mda_files), batch_size):
-            text_list = []
-            for b in range(batch_size):
-                if idx + b == len(mda_files):
-                    break
-                with open(mda_files[idx + b], 'r') as fin:
-                    text = fin.read()
-                text_list.append(text)
-            yield mda_files[idx:idx + batch_size], text_list
-    else:
-        for filename in mda_files:
-            with open(filename, 'r') as fin:
-                text = fin.read()
-
-            lines = text.splitlines()
-            lines = [ x for x in lines if re.search('[A-Z]', x) ]
-
-            for idx in range(0, len(lines), batch_size):
-                begin = idx
-                end = idx + batch_size if idx + \
-                    batch_size < len(lines) else len(lines) - 1
-                yield [filename] * (end-begin), list(range(begin, end)), lines[begin:end]
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--mda_dir', type=str, help="mda file directory")
-    parser.add_argument('-p', '--paragraph', action="store_true",
-                        help="encode by paragraph, else by document")
     parser.add_argument('-b', '--batch_size', type=int,
                         default=1024, help='batch size of model')
     parser.add_argument('-o', '--out_file', type=str, help='out file name')
     args = parser.parse_args()
 
     mda_files = glob(os.path.join(args.mda_dir, '*.mda'))
-    text_iterator = iter_file(mda_files, args.batch_size, args.paragraph)
-    header = ['mda_file'] + \
-        (['paragraph'] if args.paragraph else []) + ['2388']
+    batch_size = args.batch_size
+    out_file = args.out_file
+
+    header = ['mda_file','mean','std','25%','50%','75%']
+    fout = open(out_file,'w')
+    fout.write(','.join(header)+'\n')
+
 
     model = Model()
 
-    with open(args.out_file, 'w') as fout:
-        fout.write(','.join(header) + '\n')
-        for items in tqdm(text_iterator):
-            try:
-                text_list = items[-1]
-                
-                features = model.transform(text_list)
-                
-                list_feature = features[:, 2388].tolist()
-                
-                items = items[:-1] + tuple([list_feature])
+    for mda_file in tqdm(mda_files):
+        
+        with open(mda_file,'r') as fin:
+            text = fin.read()
+        
+        lines = list(filter(lambda x: x.strip(), text.splitlines()))
 
-                for row in zip(*items):
-                    fout.write(','.join(list(map(lambda x: str(x), row))) + '\n')
-            except:
-                print("Skipping {}...".format(items[0][0]))
+        feature_list = []
+
+        for idx in tqdm(range(0, len(lines), batch_size)):
+            line_list = lines[idx:idx+batch_size]
+
+            features = model.transform(line_list)
+
+            feature_list += features[:,2388].tolist()
+        
+        mean = np.mean(feature_list)
+        std = np.std(feature_list)
+
+        quartile1 = np.percentile(feature_list, 25)
+        median = np.median(feature_list)
+        quartile3 = np.percentile(feature_list, 75)
+
+        row = list(map(str,[ mda_file, mean, std, quartile1, median, quartile3 ]))
+
+        fout.write(','.join(row)+'\n')
+
+    fout.close()
